07120d50fb552af82ec4094600774e89478f024a
jinyoungpark00 <doghong00@naver.com>
2023-03-25 14:38:58 +0900

    week3 challenge task commit

--- posts/urls.py
+++ posts/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from posts.views import *
 
-urlpatterns = [path("", hello_world, name="hello_world")]
+urlpatterns = [
+    path("", hello_world, name="hello_world"),
+    path("introduction", introduction, name="introduction"),
+]
--- posts/views.py
+++ posts/views.py
@@ -14,3 +14,18 @@ def hello_world(request):
                 "data": "Hello world\n",
             }
         )
+
+
+def introduction(request):
+    if request.method == "GET":
+        return JsonResponse(
+            {
+                "status": 200,
+                "success": True,
+                "message": "메세지 전달 성공!",
+                "data": [
+                    {"name": "박진영", "age": 24, "major": "Economic"},
+                    {"name": "민병록", "age": 24, "major": "Software"},
+                ],
+            }
+        )
