934f1aaf66220a19eafc622d90fc98e2b7224f41
jinyoungpark00 <doghong00@naver.com>
2023-04-04 00:46:30 +0900

    week4 task commit

--- posts/urls.py
+++ posts/urls.py
@@ -1,4 +1,8 @@
 from django.urls import path
 from posts.views import *
 
-urlpatterns = [path("", hello_world, name="hello_world")]
+urlpatterns = [
+    path("", hello_world, name="hello_world"),
+    path("post_detail", get_post_all),
+    path("post_detail/<int:id>/", get_post_detail),
+]
