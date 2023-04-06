ea7b5a67a4204746205ad56e33d2fdc68dd9ef93
jinyoungpark00 <doghong00@naver.com>
2023-04-07 00:56:13 +0900

    week5 task commit

--- posts/admin.py
+++ posts/admin.py
@@ -2,5 +2,5 @@ from django.contrib import admin
 from .models import Post, Comment
 
 # Register your models here.
-admin.site.register(Post)
+admin.site.register(Post)  # 시간 추가 가능
 admin.site.register(Comment)
--- posts/urls.py
+++ posts/urls.py
@@ -3,6 +3,9 @@ from posts.views import *
 
 urlpatterns = [
     path("", hello_world, name="hello_world"),
-    path("post_detail", get_post_all),
-    path("post_detail/<int:id>/", get_post_detail),
+    path("all/", get_post_all, name="get_post_all"),
+    path("<int:id>/", post_detail, name="post_detail"),
+    path("new/", create_post, name="create_post"),
+    path("comment/<int:post_id>/", comment, name="comment"),
+    path("inTime/", get_post_in_time, name="get_post_in_time"),
 ]
