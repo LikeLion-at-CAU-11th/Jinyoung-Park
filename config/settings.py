162d80e53e4960071dfeefbee7bbcffb901e9a8b
jinyoungpark00 <doghong00@naver.com>
2023-07-28 21:30:15 +0900

    week12 standard commit

--- config/settings.py
+++ config/settings.py
@@ -79,6 +79,8 @@ THIRD_PARTY_APPS = [
     # allauth.socialaccount.providers.{소셜로그인제공업체}
     # {소셜로그인제공업체} 부분에는 구글 외에도 카카오,네이버 추가 가능
     "allauth.socialaccount.providers.google",
+    # AWS S3
+    "storages",
 ]
 
 INSTALLED_APPS = DJANGO_APPS + PROJECT_APPS + THIRD_PARTY_APPS
@@ -139,12 +141,12 @@ ALLOWED_HOSTS = ["*"]
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
-}
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
 
 
 # Password validation
@@ -205,3 +207,33 @@ SIMPLE_JWT = {
     "BLACKLIST_AFTER_ROTATION": False,  # 같은 리프레시 토큰이 반환되지 않도록
     "TOKEN_USER_CLASS": "accounts.Member",  # 사용할 db
 }
+
+
+# database
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "likelion11th",
+        "USER": "admin",
+        "PASSWORD": get_secret("DB_PASSWORD"),
+        "HOST": get_secret("DB_HOST"),
+        "PORT": "3306",
+    }
+}
+
+# AWS 권한 설정
+AWS_ACCESS_KEY_ID = get_secret("ACCESS_KEY")
+AWS_SECRET_ACCESS_KEY = get_secret("SECRET_ACCESS_KEY")
+AWS_REGION = "ap-northeast-2"
+
+# AWS S3 버킷 이름
+AWS_STORAGE_BUCKET_NAME = "likelion-11th"
+
+# AWS S3 버킷의 URL
+AWS_S3_CUSTOM_DOMAIN = "%s.s3.%s.amazonaws.com" % (AWS_STORAGE_BUCKET_NAME, AWS_REGION)
+
+AWS_S3_OBJECT_PARAMETERS = {
+    "CacheControl": "max-age=86400",
+}
+
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
